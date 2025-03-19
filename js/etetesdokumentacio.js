784aaa7506ce8adb7e733c5a3e39830a4316394b
Szabó Roland <szrtanul@gmail.com>
2025-03-19 11:50:17 +0100

    Get kérés megjelenítés ötlet

--- js/etetesdokumentacio.js
+++ js/etetesdokumentacio.js
@@ -1,11 +1,8 @@
-console.log ("Végre vagy hajtva?!!!!!!!!!!!");
 let a = document.getElementsByClassName("urlap");
 for(const ujvacsora of a){
     for(const kuld of ujvacsora.getElementsByClassName("kuld")){
         kuld.addEventListener("click", function(){
-            console.log("Belépsz ide?");
             document.getElementById("k1023").style.setProperty("display", "inherit");
-            console.log("EEEEEEEEEEEEEEEEEEEEEEEEEEEEE");
         });
     }
 }
